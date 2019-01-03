378b54f258d6f732e7916f7ed657fee4549e3887
XinZouS <sanking.saphira@foxmail.com>
2019-01-03 17:42:32 -0500

    add db setup sql

---
+++ dbSetup/db_setup.sql
@@ -0,0 +1,17 @@
+-- select Advisor, min(inceptdt) from company group by advisor -- as Advisors
+
+-- select * from blog_company
+
+select mscatid from blog_company 
+-- where advisorid_id IN (177, 282, 173) order by name
+
+
+-- == create the Advisor table == -- 
+-- insert into blog_advisors (Name)
+-- select Advisor from blog_company 
+-- where Advisor IS NOT  NULL
+-- group by Advisor 
+-- order by Advisor
+
+-- select * from blog_advisors
+
