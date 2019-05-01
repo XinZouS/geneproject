1d6f0dc1d901eb69ddc27ee56b71e0847ac6d9fc
XinZouS <sanking.saphira@foxmail.com>
2019-05-01 15:10:33 -0400

    clean up

--- blog/views.py
+++ blog/views.py
@@ -23,246 +23,14 @@ def home(request):
 	return render(request, 'blog/home.html', content)
 
 
-# @login_required
-# def advisor_table(request):
-# 	cols = ["No.", "Name", "CUSIP", "SecId", "FundId", "Advisor", "MSCat", "MSSubAdv"]
-# 	companyInfo = []
-# 	sharesAndFundsCols = ["No.", "Advisor", "FundId", "MSCat", "TR_YTD", "TR_1Y", "TR_2Y", "TR_3Y", "TR_4Y", "TR_5Y", "TR_10Y", "TR_15Y", "TR_2017", "TR_2016", "TR_2015", "TR_2014", "TR_2013", "TR_2012", "TR_2011", "TR_2010", "TR_2009", "TR_2008", "Alpha3", "Stdev3", "Beta3", "ExRet3", "Sharpe3", "InfoRat3", "R23", "QKYTD", "QK1Y", "QK2Y", "QK3Y", "QK4Y", "QK5Y", "QK10Y", "QK15Y", "QK2017", "QK2016", "QK2015", "QK2014", "QK2013", "QK2012", "QK2011", "QK2010", "QK2009", "QK2008", "QKAlph3", "QKStdv3", "QKBeta3", "QKExRt3", "QKShrp3", "QKInfR3", "QKRsq3p"]
-# 	sharesAndFunds = []
-
-# 	selectedAdvisorIds = []
-# 	selectedAdvisorNames = []
-# 	selectedMSSubAdvIds = []
-# 	selectedMSSubAdvNames = []
-# 	selectedMSCatIds = []
-# 	selectedMSCatNames = []
-# 	selectedMgrIds = []
-# 	selectedMgrNames = []
-
-# 	if request.method == "POST":
-# 		selectedAdvisorIds = intListFrom(request.POST.getlist('advId'))
-# 		selectedAdvisorNames = request.POST.getlist('advName', '')
-# 		selectedAdvisorIdSet = set(selectedAdvisorIds)
-
-# 		selectedMSSubAdvIds = intListFrom(request.POST.getlist('subId'))
-# 		selectedMSSubAdvNames = request.POST.getlist('subName', '')
-# 		selectedMSSubAdvIdSet = set(selectedMSSubAdvIds) #set(list(map(lambda x: int(x), selectedMSSubAdvIds)))
-
-# 		selectedMSCatIds = intListFrom(request.POST.getlist('catId'))
-# 		selectedMSCatNames = request.POST.getlist('catName', '')
-# 		selectedMSCatIdSet = set(selectedMSCatIds) #set(list(map(lambda x: int(x.encode("utf-8")), selectedMSCatIds)))
-
-# 		selectedMgrIds = intListFrom(request.POST.getlist('mgrId'))
-# 		selectedMgrNames = request.POST.getlist('mgrName', '')
-# 		selectedMgrIdSet = set(selectedMgrIds) #set(list(map(lambda x: int(x.encode("utf-8")), selectedMgrIds)))
-# 		print(selectedMgrIds)
-
-# 		start_time = time.time()
-# 		cmpObjByAll = []
-
-# 		if selectedAdvisorIds: # len() != 0, in pythonic
-# 			for advId in selectedAdvisorIds:
-# 				obj = Company.objects.filter(AdvisorID_id=advId).order_by("Name")
-# 				if len(obj) == 1:
-# 					cmpObjByAll.append(obj[0])
-# 				elif len(obj) > 1:
-# 					for c in obj:
-# 						cmpObjByAll.append(c)
-# 			print("--- 1.1 query Adv id: %s seconds ---" % (time.time() - start_time))
-# 			print("--- 1.1 cmpObjByAll.count = %s" % len(cmpObjByAll))
-# 			start_time = time.time()
-
-# 		isResultShouldBeEmpty = False
-
-# 		if selectedMSSubAdvIds:
-# 			if cmpObjByAll:
-# 				containMSSub = []
-# 				qualifyId = set()
-# 				for cmpObj in cmpObjByAll:
-# 					subId = cmpObj.MSSubAdvId_id
-# 					if subId in selectedMSSubAdvIdSet:
-# 						containMSSub.append(cmpObj)
-# 						qualifyId.add(subId)
-# 				print("-------- 2.0 len(qualifyId) = %s, len(SubIdSet) = %s" % (len(qualifyId), len(selectedMSSubAdvIdSet)))
-# 				# if len(qualifyId) >= len(selectedMSSubAdvIdSet):
-# 				cmpObjByAll = containMSSub
-# 				# else:
-# 				# 	print("------- !!! too much MSSubAdv selected, AND result should be nil...")
-# 				# 	cmpObjByAll = []
-# 				# isResultShouldBeEmpty = len(cmpObjByAll) == 0
-# 				print("--- 2.1 query Sub id: %s seconds ---" % (time.time() - start_time))
-# 				print("--- 2.1 cmpObjByAll.count = %s, isResultShouldBeEmpty = %s" % (len(cmpObjByAll), isResultShouldBeEmpty))
-# 				start_time = time.time()
-# 			else:
-# 				for subId in selectedMSSubAdvIds:
-# 					obj = Company.objects.filter(MSSubAdvId_id=subId).order_by("Name")
-# 					if len(obj) == 1:
-# 						cmpObjByAll.append(obj[0])
-# 					elif len(obj) > 1:
-# 						cmpObjByAll.extend(obj)
-# 				print("--- 2.2 query Sub id: %s seconds ---" % (time.time() - start_time))
-# 				print("--- 2.2 cmpObjByAll.count = %s" % len(cmpObjByAll))
-# 				start_time = time.time()
-
-# 		if selectedMSCatIds and not isResultShouldBeEmpty:
-# 			if cmpObjByAll:
-# 				containMSCat = []
-# 				qualifyId = set()
-# 				for cmpObj in cmpObjByAll:
-# 					catId = cmpObj.MSCatDbId_id
-# 					if catId in selectedMSCatIdSet:
-# 						containMSCat.append(cmpObj)
-# 						qualifyId.add(catId)
-# 				print("-------- 3.0 len(qualifyId) = %s, len(CatIdSet) = %s" % (len(qualifyId), len(selectedMSCatIdSet)))
-# 				# if len(qualifyId) >= len(selectedMSCatIdSet):
-# 				cmpObjByAll = containMSCat
-# 				# else:
-# 				# 	print("------- !!! too much MSSubAdv selected, AND result should be nil...")
-# 				# 	cmpObjByAll = []
-# 				# isResultShouldBeEmpty = len(cmpObjByAll) == 0
-# 				print("--- 3.1 filter Cat id: %s seconds ---" % (time.time() - start_time))
-# 				print("--- 3.1 cmpObjByAll.count = %s, isResultShouldBeEmpty = %s" % (len(cmpObjByAll), isResultShouldBeEmpty))
-# 				start_time = time.time()
-# 			else:
-# 				for catId in selectedMSCatIds:
-# 					obj = Company.objects.filter(MSCatDbId_id=catId).order_by("Name")
-# 					if len(obj) == 1:
-# 						cmpObjByAll.append(obj[0])
-# 					elif len(obj) > 1:
-# 						cmpObjByAll.extend(obj)
-# 				print("--- 3.2 query Cat id: %s seconds ---" % (time.time() - start_time))
-# 				print("--- 3.2 cmpObjByAll.count = %s" % len(cmpObjByAll))
-# 				start_time = time.time()
-
-# 		if selectedMgrIds and not isResultShouldBeEmpty:
-# 			if cmpObjByAll:
-# 				containMgr = []
-# 				qualifyId = set()
-# 				for cmpObj in cmpObjByAll:
-# 					mgrId = cmpObj.MgrNameId_id
-# 					if mgrId in selectedMgrIdSet:
-# 						containMgr.append(cmpObj)
-# 						qualifyId.add(mgrId)
-# 				print("-------- 4.0 len(qualifyId) = %s, len(MgrIdSet) = %s" % (len(qualifyId), len(selectedMgrIdSet)))
-# 				# if len(qualifyId) >= len(selectedMgrIdSet):
-# 				cmpObjByAll = containMgr
-# 				# else:
-# 				# 	print("------- !!! too much MgrNames selected, AND result should be nil...")
-# 				# 	cmpObjByAll = []
-# 				# isResultShouldBeEmpty = len(cmpObjByAll) == 0
-# 				print("--- 4.1 filter Cat id: %s seconds ---" % (time.time() - start_time))
-# 				print("--- 4.1 cmpObjByAll.count = %s" % len(cmpObjByAll))
-# 				start_time = time.time()
-# 			else:
-# 				for mgrId in selectedMgrIds:
-# 					obj = Company.objects.filter(MgrNameId_id=mgrId).order_by("Name")
-# 					if len(obj) == 1:
-# 						cmpObjByAll.append(obj[0])
-# 					elif len(obj) > 1:
-# 						cmpObjByAll.extend(obj)
-# 				print("--- 4.2 query Mgr id: %s seconds ---" % (time.time() - start_time))
-# 				print("--- 4.2 cmpObjByAll.count = %s" % len(cmpObjByAll))
-# 				start_time = time.time()
-
-# 		# if selectedAdvisorIds and not isResultShouldBeEmpty: # filter out the first selection result;
-# 		# 	qualifyId = set()
-# 		# 	for cmpObj in cmpObjByAll:
-# 		# 		qualifyId.add(cmpObj.AdvisorID_id)
-# 		# 	if len(qualifyId) < len(selectedAdvisorIdSet):
-# 		# 		print("------- 4.0 !!! too much AdvisorID_id selected, AND result should be nil...")
-# 		# 		cmpObjByAll = []
-
-# 		companyInfo = cmpObjByAll
-
-# 		# for tag Performance
-# 		sharesAndFunds = getPerformanceInfo(companyInfo)
-
-# 		selectedAdvisorNameset = set(selectedAdvisorNames)
-# 		selectedMSSubAdvNameSet = set(selectedMSSubAdvNames)
-# 		selectedMSCatNameSet = set(selectedMSCatNames)
-# 		selectedMgrNameSet = set(selectedMgrNames)
-
-# 		selectedAdvisorNames = []
-# 		for advName in selectedAdvisorNameset:
-# 			selectedAdvisorNames.append(advName.encode("utf-8"))
-
-# 		selectedMSSubAdvNames = []
-# 		for advName in selectedMSSubAdvNameSet:
-# 			selectedMSSubAdvNames.append(advName.encode("utf-8"))
-
-# 		selectedMSCatNames = []
-# 		for catName in selectedMSCatNameSet:
-# 			selectedMSCatNames.append(catName.encode("utf-8"))
-
-# 		selectedMgrNames = []
-# 		for mgrName in selectedMgrNameSet:
-# 			selectedMgrNames.append(mgrName.encode("utf-8"))
-
-# 	else:
-# 		print "--- [GET] request is GET, init page....,"
-
-# 	# for Auto-complete search lists
-# 	advs = Advisors.objects.all()
-# 	subs = MSSubAdvs.objects.all()
-# 	cats = MSCats.objects.all()
-# 	mgrs = MgrNames.objects.all()
-# 	advsNames 	= list(map(lambda x: x.Name.encode("utf-8"), advs))
-# 	subNames 	= list(map(lambda x: x.Name.encode("utf-8"), subs))
-# 	mscatNames  = list(map(lambda x: x.Name.encode("utf-8"), cats))
-# 	mgrNames    = list(map(lambda x: x.Name.encode("utf-8"), mgrs))
-
-# 	idOfAdvisorNameDict = {} # {'Name':'advId'}
-# 	idOfMSSubAdvNameDict = {}
-# 	idOfMSCatNameDict = {}
-# 	idOfMgrNameDict = {}
-
-# 	for adv in advs:
-# 		idOfAdvisorNameDict[adv.Name.encode("utf-8")] = adv.id
-# 	for sub in subs:
-# 		idOfMSSubAdvNameDict[sub.Name.encode("utf-8")] = sub.id
-# 	for cat in cats:
-# 		idOfMSCatNameDict[cat.Name.encode("utf-8")] = cat.id
-# 	for mgr in mgrs:
-# 		idOfMgrNameDict[mgr.Name.encode("utf-8")] = mgr.id
-
-
-# 	content = {
-# 		'companys': companyInfo, # get first n rows 
-# 		'colnames': cols,
-
-# 		'advisorNames': advsNames,
-# 		'idOfAdvisorNameDict':  idOfAdvisorNameDict,
-# 		'selectedAdvisorIds':   selectedAdvisorIds,
-# 		'selectedAdvisorNames': selectedAdvisorNames,
-
-# 		'mssubadvNames': subNames,
-# 		'idOfMSSubAdvNameDict':  idOfMSSubAdvNameDict,
-# 		'selectedMSSubAdvIds':   selectedMSSubAdvIds,
-# 		'selectedMSSubAdvNames': selectedMSSubAdvNames,
-
-# 		'mscatNames': mscatNames,
-# 		'idOfMSCatNameDict' : idOfMSCatNameDict,
-# 		'selectedMSCatIds':   selectedMSCatIds,
-# 		'selectedMSCatNames': selectedMSCatNames,
-
-# 		'mgrNames': mgrNames,
-# 		'idOfMgrNameDict' : idOfMgrNameDict,
-# 		'selectedMgrIds':   selectedMgrIds,
-# 		'selectedMgrNames': selectedMgrNames,
-
-# 		'sharesAndFunds': sharesAndFunds, # for tag Performance
-# 		'sharesAndFundsColnames': sharesAndFundsCols,
-# 	}
-
-# 	return render(request, 'blog/advisor_table.html', content)
-
 
 @login_required
 def fit_default(request):
-	cols = ["No.","Name","Category","MF/VA","SubAdvised","Index Fund","Watch List","VIT","Fund of Funds","Inception Date","Advisor","Subadvisor","AUM","NFYTD","NF2017","NF2016","SubStart","SubSched","SubAdvEffRate","Sub_Rate($)","TR_YTD","TR_1Y","TR_2Y","TR_3Y","TR_5Y","TR_10Y","Alpha","ExcessRet","Sharpe","InfoRat","Beta","Stdev","R2","UpsideCap","DownsideCap","TrackingErr","QRK_YTD","QRK_1Y","QRK_2Y","QRK_3Y","QRK_5Y","QRK_10Y","QRK_15Y","QRK_Alpha","QRK_ExcessRet","QRK_Sharpe","QRK_InfoRat","QRK_Beta","QRK_Stdev","QRK_R2","QRK_UpsideCap","QRK_DownsideCap","QRK_TrackingErr","Team Managed","Prospectus Net Expense Ratio","Manager Name","Manager Tenure (Longest)","Manager Tenure (Average)","Benchmark"]
+	defaultCols = ["No.","Name","Category","MF/VA","SubAdvised","Index Fund","Watch List","VIT","Fund of Funds","Inception Date","Advisor","Subadvisor","AUM","NFYTD","NF2017","NF2016","SubStart","SubSched","SubAdvEffRate","Sub_Rate($)","TR_YTD","TR_1Y","TR_2Y","TR_3Y","TR_5Y","TR_10Y","Alpha","ExcessRet","Sharpe","InfoRat","Beta","Stdev","R2","UpsideCap","DownsideCap","TrackingErr","QRK_YTD","QRK_1Y","QRK_2Y","QRK_3Y","QRK_5Y","QRK_10Y","QRK_15Y","QRK_Alpha","QRK_ExcessRet","QRK_Sharpe","QRK_InfoRat","QRK_Beta","QRK_Stdev","QRK_R2","QRK_UpsideCap","QRK_DownsideCap","QRK_TrackingErr","Team Managed","Prospectus Net Expense Ratio","Manager Name","Manager Tenure (Longest)","Manager Tenure (Average)","Benchmark"]
 	companyInfo = []
 	sharesAndFundsCols = ["No.", "Advisor", "FundId", "MSCat", "TR_YTD", "TR_1Y", "TR_2Y", "TR_3Y", "TR_4Y", "TR_5Y", "TR_10Y", "TR_15Y", "TR_2017", "TR_2016", "TR_2015", "TR_2014", "TR_2013", "TR_2012", "TR_2011", "TR_2010", "TR_2009", "TR_2008", "Alpha3", "Stdev3", "Beta3", "ExRet3", "Sharpe3", "InfoRat3", "R23", "QKYTD", "QK1Y", "QK2Y", "QK3Y", "QK4Y", "QK5Y", "QK10Y", "QK15Y", "QK2017", "QK2016", "QK2015", "QK2014", "QK2013", "QK2012", "QK2011", "QK2010", "QK2009", "QK2008", "QKAlph3", "QKStdv3", "QKBeta3", "QKExRt3", "QKShrp3", "QKInfR3", "QKRsq3p"]
 	sharesAndFunds = []
+
 
 	selectedAdvisorIds = []
 	selectedAdvisorNames = []
@@ -276,19 +44,15 @@ def fit_default(request):
 	if request.method == "POST":
 		selectedAdvisorIds = intListFrom(request.POST.getlist('advId'))
 		selectedAdvisorNames = request.POST.getlist('advName', '')
-		selectedAdvisorIdSet = set(selectedAdvisorIds)
 
 		selectedMSSubAdvIds = intListFrom(request.POST.getlist('subId'))
 		selectedMSSubAdvNames = request.POST.getlist('subName', '')
-		selectedMSSubAdvIdSet = set(selectedMSSubAdvIds) #set(list(map(lambda x: int(x), selectedMSSubAdvIds)))
 
 		selectedMSCatIds = intListFrom(request.POST.getlist('catId'))
 		selectedMSCatNames = request.POST.getlist('catName', '')
-		selectedMSCatIdSet = set(selectedMSCatIds) #set(list(map(lambda x: int(x.encode("utf-8")), selectedMSCatIds)))
 
 		selectedMgrIds = intListFrom(request.POST.getlist('mgrId'))
 		selectedMgrNames = request.POST.getlist('mgrName', '')
-		selectedMgrIdSet = set(selectedMgrIds) #set(list(map(lambda x: int(x.encode("utf-8")), selectedMgrIds)))
 
 		start_time = time.time()
 		cmpObjByAll = []
@@ -335,12 +99,10 @@ def fit_default(request):
 	subNames 	= list(map(lambda x: x.Name.encode("utf-8"), subs))
 	mscatNames  = list(map(lambda x: x.Name.encode("utf-8"), cats))
 	mgrNames    = list(map(lambda x: x.Name.encode("utf-8"), mgrs))
-
 	idOfAdvisorNameDict = {} # {'Name':'advId'}
 	idOfMSSubAdvNameDict = {}
 	idOfMSCatNameDict = {}
 	idOfMgrNameDict = {}
-
 	for adv in advs:
 		idOfAdvisorNameDict[adv.Name.encode("utf-8")] = adv.id
 	for sub in subs:
@@ -353,7 +115,7 @@ def fit_default(request):
 
 	content = {
 		'companys': companyInfo, # get first n rows 
-		'colnames': cols,
+		'colnames': defaultCols,
 
 		'advisorNames': advsNames,
 		'idOfAdvisorNameDict':  idOfAdvisorNameDict,
