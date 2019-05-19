560942a69a71366be31001c0491305cfc141861d
XinZouS <sanking.saphira@foxmail.com>
2019-05-19 17:04:34 -0400

    update detail html

--- strategy/urls.py
+++ strategy/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 	url(r'^$',views.StrategyList.as_view(),name='all'),
 	url(r'^new/$',views.StrategyCreate.as_view(),name='create'),
 	url(r'^by/(?P<username>[-\w]+)/$',views.StrategyListByUser.as_view(),name='by_user'),
-	url(r'^detail/(?P<slug>[-\w]+)/(?P<pk>\d+)/$',views.StrategyDetail.as_view(),name='detail'),
+	url(r'^detail/(?P<pk>\d+)/(?P<slug>[-\w]+)/$',views.StrategyDetail.as_view(),name='detail'),
 	url(r'^delete/(?P<pk>\d+)/$',views.StrategyDelete.as_view(),name='delete'),
 ]
 
